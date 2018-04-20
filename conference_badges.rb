add569c3f7724b26467297a0ec8b4bf2356d9d0d
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:20:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-room = [1, 2, 3, 4, 5, 6, 7]
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#room = [1, 2, 3, 4, 5, 6, 7]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
