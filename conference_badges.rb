80053ccfe9e2d1ffb938f70eaf332fe4fbe32acd
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:50:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,5 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  puts batch_badge_creator(speakers)
-  puts assign_rooms(speakers)
+  speakers.each { |speakers| puts batch_badge_creator(speakers) puts assign_rooms(speakers) }
 end
