9ffb1f3289201dbe208b8babd49b8380aa9b4a4e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:17:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,6 @@ def badge_maker
 end
 
 def batch_badge_creator
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |name|
     puts "Hello, my name is #{name}!"
   end
