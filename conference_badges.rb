32e36a50156422e496e79ce82655c1e6b1ee7b1b
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:12:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,6 @@ end
 
 def assign_rooms
   room = [1, 2, 3, 4, 5, 6, 7]
-  puts "Hello, {speakers}! You'll be assigned to #{room}!"
+  room.each_with_index do |num|
+  puts "Hello, #{speakers}! You'll be assigned to #{room}!"
 end
