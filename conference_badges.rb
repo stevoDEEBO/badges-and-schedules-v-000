db48530a7c938a4903d60812a2176c2ba9038f25
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:17:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = [1, 2, 3, 4, 5, 6, 7]
+
+def badge_maker
+  speakers.each do |name|
+    puts "Hello, my name is #{name}!"
+  end
+
 def batch_badge_creator
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |name|
@@ -6,7 +14,6 @@ def batch_badge_creator
 end
 
 def assign_rooms
-  room = [1, 2, 3, 4, 5, 6, 7]
   room.each_with_index do |num|
     puts "Hello, #{speakers}! You'll be assigned to #{room}!"
   end
