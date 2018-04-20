f0da9733060d984b3bdb77a9b5c0d3b1c763f2f0
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-20 19:35:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(speakers)
   badges
 end
 
-def assign_rooms(speakers, room)
+def assign_rooms(speakers)
   rooms = room + 1
   rm_assigns = []
   speakers.each_with_index { |speakers, rooms| puts rm_assigns << "Hello, #{speakers}! You'll be assigned to #{rooms}!" }
